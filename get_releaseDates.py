8ff4517b80b7432463ea34520587e19ac8c0a917
Patrick Lafferty <patrick.lafferty@slalom.com>
2019-06-24 15:25:17 -0400

    Documented get_releaseDates.py

--- get_releaseDates.py
+++ get_releaseDates.py
@@ -28,20 +28,25 @@ base_url = "https://api.musixmatch.com/ws/1.1/"
 api_match_endpoint = "matcher.track.get"
 api_album_endpoint = "album.get"
 format_url = "?format=json&callback=callback"
-api_key = "&apikey=102da159729cf7e66c9be065ef0d88b0"        #c56c251612909ec56b07b9411bd136b8"
+api_key = "&apikey=" + config['mxm_api_key']
 
-sqlConnStr = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server}; SERVER='+config['db_server']+'; DATABASE='+config['db']+'; UID='+config['username']+'; PWD='+config['password'])
+# Connect to SQL DB
+sqlConnStr = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server}; SERVER='+config['db_server']+'; DATABASE='+config['db']+'; UID='+config['db_username']+'; PWD='+config['db_password'])
 cursor = sqlConnStr.cursor()
 
+# Query DB for all records without a releaseDate
 df = pd.read_sql("SELECT [songKey],[songName],[artistName] FROM dbo.Hot100 WHERE [releaseDate] IS NULL", sqlConnStr)
 
+# Print Status
 print(' Getting and Assigning Track Release Dates...')
 l=len(df)
 printProgressBar(0,l)
 
 for index,row in df.iterrows():
+
     printProgressBar(index, l)
-    # Construct first API call, matcher.track.get, to get the MXM track_id
+
+    # Construct first MXM API call, matcher.track.get, to get album ID
     api_match_call = base_url + api_match_endpoint + format_url + "&q_artist=" + row['artistName'].replace('&', '') + "&q_track=" + row['songName'].replace('&', '') + api_key
     request = requests.get(api_match_call)
     mxm_match = request.json()
@@ -53,6 +58,7 @@ for index,row in df.iterrows():
         print(api_match_call)
         print(mxm_match)
 
+    # Construct second MXM API call, album.get, to get album release date and name
     api_album_call = base_url + api_album_endpoint + format_url + "&album_id=" + str(album_id) + api_key
     album_request = requests.get(api_album_call)
     mxm_album = album_request.json()
@@ -65,11 +71,13 @@ for index,row in df.iterrows():
         print(api_album_call)
         print(mxm_album)
 
+    # Inspect release date, as some come as only year or month. If this is the case, assign the first of the month or the first of the year to construct a full date.
     if len(album_release_date) == 7:
         album_release_date += '-01'
     if len(album_release_date) == 4:
         album_release_date += '-01-01'
 
+    # Update DB record with new album data
     try:
         cursor.execute("UPDATE dbo.Hot100 SET [albumName] = ?, [releaseDate] = ? WHERE [songKey] = ?", album_name,album_release_date,row['songKey']) 
         sqlConnStr.commit()
@@ -81,7 +89,9 @@ for index,row in df.iterrows():
 
 printProgressBar(l,l)
 
+# Close SQL Connection
 cursor.close()
 sqlConnStr.close()
 
+# Print Script Results
 print(" Script run time: " + str(datetime.now() - start_time))
